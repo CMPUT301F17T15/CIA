026192a24d73d84dd44158111410513263d49c0f
amalik2 <619mwa@gmail.com>
2017-10-16 21:01:15 -0600

    elasticsearch

--- app/src/main/java/com/cmput301/cia/utilities/ElasticSearchUtilities.java
+++ app/src/main/java/com/cmput301/cia/utilities/ElasticSearchUtilities.java
@@ -1,11 +1,101 @@
 package com.cmput301.cia.utilities;
 
+import com.searchly.jestdroid.JestDroidClient;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.searchbox.core.DocumentResult;
+import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+
 /**
  * Created by Adil on Oct 13 2017.
  */
 
+
 public class ElasticSearchUtilities {
 
     private static final String INDEX = "CMPUT301F17T15";
+
+    private static JestDroidClient client;
+
+    // Adds tweets to elastic search
+    // Get=NormalTweet, Progress=Void, Result=Void
+    /*public static class AddTweetsTask extends AsyncTask<NormalTweet, Void, Void> {
+
+        @Override
+        protected Void doInBackground(NormalTweet... tweets) {
+            verifySettings();
+
+            for (NormalTweet tweet : tweets) {
+                Index index = new Index.Builder(tweet).index("testing").type("tweet").build();
+
+                try {
+                    // where is the client?
+                    DocumentResult execute = client.execute(index);
+                    if (execute.isSucceeded()){
+                        tweet.setId(execute.getId());
+                    }
+                }
+                catch (Exception e) {
+                    Log.i("Error", "The application failed to build and send the tweets");
+                }
+
+            }
+            return null;
+        }
+    }
+
+    // Gets tweets from elastic search
+    public static class GetTweetsTask extends AsyncTask<String, Void, ArrayList<NormalTweet>> {
+        @Override
+        protected ArrayList<NormalTweet> doInBackground(String... search_parameters) {
+            verifySettings();
+
+            ArrayList<NormalTweet> tweets = new ArrayList<NormalTweet>();
+
+            // Build the query
+
+            String query = "{\n" +
+                    "    \"query\": {\"term\": {\"message\":\"" + search_parameters[0] + "\"}}\n" + "}";
+
+
+            Search search = new Search.Builder(query).addIndex("testing").addType("tweet").build();
+
+            try {
+                SearchResult result = client.execute(search);
+                if (result.isSucceeded()) {
+                    List<NormalTweet> foundTweets = result.getSourceAsObjectList(NormalTweet.class);
+                    tweets.addAll(foundTweets);
+                } else {
+                    Log.i("Error", "the search query failed to find any tweets that matched");
+                }
+            }
+            catch (Exception e) {
+                Log.i("Error", "Something went wrong when we tried to communicate with the elasticsearch server!");
+            }
+
+            return tweets;
+        }
+    }*/
+
+    public static void verifySettings() {
+        if (client == null) {
+            DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080");
+            DroidClientConfig config = builder.build();
+
+            JestClientFactory factory = new JestClientFactory();
+            factory.setDroidClientConfig(config);
+            client = (JestDroidClient) factory.getObject();
+        }
+    }
 
 }
