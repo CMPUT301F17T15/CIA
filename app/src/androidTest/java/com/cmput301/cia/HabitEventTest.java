26e11015a909e6b1bd89b5b9d8430a48362755c9
Jessica Prieto <jessicaprieto31@gmail.com>
2017-10-22 10:39:32 -0600

    modified the model for HabitEvent to have a base64encoded string for image; added test case for getBase64EncodedPhoto; refactored EditHabitTest

--- app/src/androidTest/java/com/cmput301/cia/HabitEventTest.java
+++ app/src/androidTest/java/com/cmput301/cia/HabitEventTest.java
@@ -6,12 +6,14 @@ package com.cmput301.cia;
 
 import android.location.Location;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Base64;
 
 import com.cmput301.cia.models.HabitEvent;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
@@ -36,8 +38,12 @@ public class HabitEventTest {
      */
     @Test
     public void testGettersOnBaseConstructor() {
-        HabitEvent habitEvent = new HabitEvent("comment", null);
-        assertEquals("comment", habitEvent.getComment());
+        String testBase64Image = getMockImage();
+
+        HabitEvent habitEvent = new HabitEvent("comment", testBase64Image);
+
+        assertEquals("wrong comment", "comment", habitEvent.getComment());
+        assertEquals("wrong image", testBase64Image, habitEvent.getBase64EncodedPhoto());
     }
 
     /** testing method(s): getComment(), getDate()
@@ -47,9 +53,12 @@ public class HabitEventTest {
     @Test
     public void testGetCommentsTestWithDateParams() {
         Date testDate = new Date();
+        String testBase64Image = getMockImage();
 
-        HabitEvent habitEvent = new HabitEvent("comment", null, testDate);
+        HabitEvent habitEvent = new HabitEvent("comment", testBase64Image, testDate);
+
         assertEquals("wrong comment", "comment", habitEvent.getComment());
+        assertEquals("wrong image", testBase64Image, habitEvent.getBase64EncodedPhoto());
         assertEquals("wrong date", testDate, habitEvent.getDate());
     }
 
@@ -60,14 +69,37 @@ public class HabitEventTest {
     @Test
     public void testGetCommentsTestWithDateAndLocationParams() {
         Date testDate = new Date();
+        String testBase64Image = getMockImage();
+        Location testLocation = getMockLocation();
 
+        HabitEvent habitEvent = new HabitEvent("comment", testBase64Image, testDate, testLocation);
+
+        assertEquals("wrong comment", "comment", habitEvent.getComment());
+        assertEquals("wrong image", testBase64Image, habitEvent.getBase64EncodedPhoto());
+        assertEquals("wrong date", testDate, habitEvent.getDate());
+        assertEquals("wrong location", testLocation, habitEvent.getLocation());
+    }
+
+    /**
+     * method for creating a mock image to test habitEvent.getBase64EncodedPhoto()
+     * @return
+     */
+    public String getMockImage() {
+        String text = "test image string";
+        byte[] data = new byte[0];
+        try {
+            data = text.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String base64 = Base64.encodeToString(data, Base64.DEFAULT);
+        return base64;
+    }
+
+    public Location getMockLocation() {
         Location testLocation = new Location("");
         testLocation.setLatitude(53.525283d);
         testLocation.setLongitude(-113.525612d);
-
-        HabitEvent habitEvent = new HabitEvent("comment", null, testDate, testLocation);
-        assertEquals("comment", habitEvent.getComment());
-        assertEquals("wrong date", testDate, habitEvent.getDate());
-        assertEquals("wrong location", testLocation, habitEvent.getLocation());
+        return testLocation;
     }
 }
--- app/src/main/java/com/cmput301/cia/models/HabitEvent.java
+++ app/src/main/java/com/cmput301/cia/models/HabitEvent.java
@@ -11,8 +11,9 @@ import java.io.Serializable;
 import java.util.Date;
 
 /**
- * Version 1
+ * Version 1.2
  * Author: Adil Malik
+ * Modified by: Jessica Prieto
  * Date: Oct 13 2017
  *
  * Represents a habit event that the user has successfully completed
@@ -21,7 +22,7 @@ import java.util.Date;
 public class HabitEvent implements Serializable {
 
     private String comment;
-    private Image photo;
+    private String base64EncodedPhoto;
     private Date date;
 
     private Location location;
@@ -29,11 +30,11 @@ public class HabitEvent implements Serializable {
     /**
      * Construct a new habit event
      * @param comment the optional habit comment (not null)
-     * @param image a photo of the event
+     * @param image a base64 encoded photo of the event
      */
-    public HabitEvent(String comment, Image image){
+    public HabitEvent(String comment, String image){
         this.comment = comment;
-        photo = image;
+        base64EncodedPhoto = image;
         date = new Date();
         location = null;
     }
@@ -41,12 +42,12 @@ public class HabitEvent implements Serializable {
     /**
      * Construct a new habit event
      * @param comment the optional habit comment
-     * @param image a photo of the event
+     * @param image a base64 encoded photo of the event
      * @param date the date the event occurred on
      */
-    public HabitEvent(String comment, Image image, Date date){
+    public HabitEvent(String comment, String image, Date date){
         this.comment = comment;
-        photo = image;
+        base64EncodedPhoto = image;
         this.date = date;
         location = null;
     }
@@ -55,13 +56,13 @@ public class HabitEvent implements Serializable {
      *
      * Construct a new habit event
      * @param comment the optional habit comment
-     * @param photo a photo of the event
+     * @param image a base64EncodedPhoto of the event
      * @param date the date the event occurred on
      * @param location the location where the event occurred
      */
-    public HabitEvent(String comment, Image photo, Date date, Location location) {
+    public HabitEvent(String comment, String image, Date date, Location location) {
         this.comment = comment;
-        this.photo = photo;
+        this.base64EncodedPhoto = image;
         this.date = date;
         this.location = location;
     }
@@ -74,12 +75,12 @@ public class HabitEvent implements Serializable {
         this.comment = comment;
     }
 
-    public Image getPhoto() {
-        return photo;
+    public String getBase64EncodedPhoto() {
+        return base64EncodedPhoto;
     }
 
-    public void setPhoto(Image photo) {
-        this.photo = photo;
+    public void setBase64EncodedPhoto(String base64EncodedPhoto) {
+        this.base64EncodedPhoto = base64EncodedPhoto;
     }
 
     public Date getDate() {
