754c5299c7586187b969d0759d1830f2f1b4382a
Jessica Prieto <jessicaprieto31@gmail.com>
2017-12-01 12:07:08 -0700

    dashboard is now able to get today's tasks

--- app/src/main/java/com/cmput301/cia/activities/TodaysHabitsFragment.java
+++ app/src/main/java/com/cmput301/cia/activities/TodaysHabitsFragment.java
@@ -4,14 +4,23 @@
 
 package com.cmput301.cia.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.cmput301.cia.R;
+import com.cmput301.cia.activities.events.HistoryActivity;
+import com.cmput301.cia.controller.CheckableListViewAdapter;
+import com.cmput301.cia.controller.TimedClickListener;
+import com.cmput301.cia.models.Habit;
 import com.cmput301.cia.models.Profile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jessica on 2017-12-01.
@@ -19,6 +28,13 @@ import com.cmput301.cia.models.Profile;
 
 public class TodaysHabitsFragment extends Fragment {
     public static final String ID_PROFILE = "User";
+    // the habits the user must do today
+    private List<Habit> todaysHabits;
+    private Profile user;
+    private ListView checkable;
+    private CheckableListViewAdapter checkableAdapter;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         // Defines the xml file for the fragment
@@ -27,7 +43,13 @@ public class TodaysHabitsFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+        user = (Profile) getArguments().getSerializable(ID_PROFILE);
+        todaysHabits = user.getTodaysHabits();
 
+        // today's tasks listview (checkable)
+        checkable = (ListView) view.findViewById(R.id.TodayToDoListView);
+        checkable.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        resetCheckableListAdapter();
     }
     public static Fragment create(Profile user) {
         TodaysHabitsFragment todaysHabitsFragment = new TodaysHabitsFragment();
@@ -36,5 +58,13 @@ public class TodaysHabitsFragment extends Fragment {
         todaysHabitsFragment.setArguments(args);
 
         return todaysHabitsFragment;
+    }
+
+    /**
+     * Update the checkable list view adapter for the "today's tasks" list
+     */
+    private void resetCheckableListAdapter(){
+        checkableAdapter = new CheckableListViewAdapter(getContext(), R.layout.checkable_list_view, R.id.CheckedTextView, todaysHabits);
+        checkable.setAdapter(checkableAdapter);
     }
 }
