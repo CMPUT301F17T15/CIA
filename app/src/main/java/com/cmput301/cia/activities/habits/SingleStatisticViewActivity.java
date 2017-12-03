4d6210bcc9bfa4d3207c511f3c448718fa9ead26
amalik2 <619mwa@gmail.com>
2017-12-03 01:37:10 -0700

    refactoring for cleaner code

--- app/src/main/java/com/cmput301/cia/activities/habits/SingleStatisticViewActivity.java
+++ app/src/main/java/com/cmput301/cia/activities/habits/SingleStatisticViewActivity.java
@@ -63,7 +63,8 @@ public class SingleStatisticViewActivity extends AppCompatActivity {
         //Information receiving from previous activity
         Habit habit = (Habit)getIntent().getSerializableExtra(ID_HABIT);
         TextView completed = (TextView) findViewById(R.id.CompletedTextView);
-        completed.setText("" + habit.getTimesCompleted());
+        int completions = habit.getTimesCompleted();
+        completed.setText("" + completions);
 
         lastComplete = (TextView) findViewById(R.id.LastCompleteTextView);
 
@@ -73,87 +74,15 @@ public class SingleStatisticViewActivity extends AppCompatActivity {
             lastComplete.setText("Never");
 
         TextView miss = (TextView) findViewById(R.id.MissedTextView);
-        miss.setText("" + habit.getTimesMissed());
+        int misses = habit.getTimesMissed();
+        miss.setText("" + misses);
 
         TextView title = (TextView) findViewById(R.id.HabitNameTextView);
         title.setText(habit.getTitle());
 
-        SimpleDateFormat formatter = new SimpleDateFormat("MMM dd");
-
-        List<String> missedHabitDates = new ArrayList<>();
-        List<String> completedHabitDates = new ArrayList<>();
-        for (HabitEvent event : habit.getEvents())
-            completedHabitDates.add(formatter.format(event.getDate()));
-        for (Date date : habit.getMissedDates())
-            missedHabitDates.add(formatter.format(date));
-
-        Collections.sort(missedHabitDates);
-        Collections.sort(completedHabitDates);
-
-        //Data processing for line chart
-        List<String> xAxis = new ArrayList<>();
-
-        List<Date> dates = new ArrayList<>();
-        Calendar calendar = new GregorianCalendar();
-        calendar.set(2016, 10, 10);
-        habit.setStartDate(calendar.getTime());
-        calendar.setTime(habit.getStartDate());
-        Date end = new Date();
-        while (!DateUtilities.isSameDay(calendar.getTime(), end) && DateUtilities.isBefore(calendar.getTime(), end)){
-            dates.add(calendar.getTime());
-            xAxis.add(formatter.format(calendar.getTime()));
-            calendar.add(Calendar.DATE, 1);
-        }
-
-        int numDataPoints = xAxis.size();
-        ArrayList<Entry> yxexs = new ArrayList<>();
-        Float[] yAxis = new Float[numDataPoints];
-
-        int completions = 0;
-        int misses = 0;
-
-        for (int i = 0; i < dates.size(); ++i){
-            if (Collections.binarySearch(missedHabitDates, formatter.format(dates.get(i))) >= 0){
-                ++misses;
-            } else if (Collections.binarySearch(completedHabitDates, formatter.format(dates.get(i))) >= 0){
-                ++completions;
-            }
-
-            float percentage = 0.0f;
-            if (completions + misses != 0)
-                percentage = (completions / (float)(completions + misses)) * 100;
-
-            yAxis[i] = percentage;
-        }
-
-        int pos = 0;
-        for (float i : yAxis) {
-            yxexs.add(new Entry(pos, i));
-            pos = pos + 1;
-        }
-
-        String[] xaxes = new String[xAxis.size()];
-        for (int i = 0; i < xAxis.size(); i++) {
-            xaxes[i] = xAxis.get(i).toString();
-        }
-        XAxis xax = lineChart.getXAxis();
-        xax.setValueFormatter(new XValueFormatter(xaxes));
-        xax.setGranularity(1);
-        xax.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
-        xax.setTextSize(10f);
-
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        LineDataSet lineDataSet = new LineDataSet(yxexs, "Progress(1=completed, -1=missed)");
-        lineDataSet.setDrawCircles(false);
-        lineDataSet.setColor(Color.BLUE);
-        lineDataSet.setLineWidth(3f);
-        lineDataSet.setValueTextSize(15f);
-        lineDataSet.setValueTextColor(Color.RED);
-
-        dataSets.add(lineDataSet);
-        lineChart.setData(new LineData(dataSets));
-        lineChart.setVisibleXRangeMaximum(30);
-        lineChart.getLineData().setDrawValues(false);
+        // error if there is no data whatsoever for the chart
+        if (completions + misses != 0)
+            initChartData(habit);
     }
 
     /**
@@ -172,6 +101,9 @@ public class SingleStatisticViewActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Initialize the non-data aspects of the line chart
+     */
     private void initChart(){
 
         //Initial line Chart for visualization of statistic
@@ -187,5 +119,96 @@ public class SingleStatisticViewActivity extends AppCompatActivity {
         leftAxis.setAxisMinimum(0.0f);
         leftAxis.enableGridDashedLine(10f, 10f, 0f);
         leftAxis.setDrawLimitLinesBehindData(true);
+    }
+
+    /**
+     * Fill the chart with data
+     * @param habit the habit to get data from
+     */
+    private void initChartData(Habit habit){
+
+        // date formatter for values on the x-axis
+        SimpleDateFormat formatter = new SimpleDateFormat("MMM dd YY");
+
+        //Data processing for line chart
+        List<String> xAxis = new ArrayList<>();
+
+        // all dates since the habit was supposed to start, not including today
+        List<Date> dates = new ArrayList<>();
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTime(habit.getStartDate());
+        Date end = new Date();
+        while (!DateUtilities.isSameDay(calendar.getTime(), end) && DateUtilities.isBefore(calendar.getTime(), end)){
+            dates.add(calendar.getTime());
+            xAxis.add(formatter.format(calendar.getTime()));
+            calendar.add(Calendar.DATE, 1);
+        }
+
+        int numDataPoints = xAxis.size();
+        ArrayList<Entry> yxexs = new ArrayList<>();
+        Float[] yAxis = new Float[numDataPoints];
+
+        // Keep track of all dates the habit was missed and completed on, to calculate percentages in between dates
+        List<String> missedHabitDates = new ArrayList<>();
+        List<String> completedHabitDates = new ArrayList<>();
+        for (HabitEvent event : habit.getEvents())
+            completedHabitDates.add(formatter.format(event.getDate()));
+        for (Date date : habit.getMissedDates())
+            missedHabitDates.add(formatter.format(date));
+
+        // sort for binary search
+        Collections.sort(missedHabitDates);
+        Collections.sort(completedHabitDates);
+
+        // number of completions thus far
+        int completions = 0;
+        // number of times the habit was missed
+        int misses = 0;
+
+        // go through each date since the habit was started
+        for (int i = 0; i < dates.size(); ++i){
+            if (Collections.binarySearch(missedHabitDates, formatter.format(dates.get(i))) >= 0){
+                ++misses;
+            } else if (Collections.binarySearch(completedHabitDates, formatter.format(dates.get(i))) >= 0){
+                ++completions;
+            }
+
+            float percentage = 0.0f;
+            // prevent division by 0 error
+            if (completions + misses != 0)
+                percentage = (completions / (float)(completions + misses)) * 100;
+
+            yAxis[i] = percentage;
+        }
+
+        int pos = 0;
+        for (float i : yAxis) {
+            yxexs.add(new Entry(pos, i));
+            pos = pos + 1;
+        }
+
+        String[] xaxes = new String[xAxis.size()];
+        for (int i = 0; i < xAxis.size(); i++) {
+            xaxes[i] = xAxis.get(i).toString();
+        }
+
+        XAxis xax = lineChart.getXAxis();
+        xax.setValueFormatter(new XValueFormatter(xaxes));
+        xax.setGranularity(1);
+        xax.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+        xax.setTextSize(10f);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        LineDataSet lineDataSet = new LineDataSet(yxexs, "Progress(1=completed, -1=missed)");
+        lineDataSet.setDrawCircles(false);
+        lineDataSet.setColor(Color.BLUE);
+        lineDataSet.setLineWidth(3f);
+        lineDataSet.setValueTextSize(15f);
+        lineDataSet.setValueTextColor(Color.RED);
+
+        dataSets.add(lineDataSet);
+        lineChart.setData(new LineData(dataSets));
+        lineChart.setVisibleXRangeMaximum(30);
+        lineChart.getLineData().setDrawValues(false);
     }
 }
