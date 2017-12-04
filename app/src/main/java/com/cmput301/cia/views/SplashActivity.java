5c1e63ed3406cbc352cddc3925ec805a6c617b01
Jessica Prieto <jessicaprieto31@gmail.com>
2017-12-04 14:04:40 -0700

    fixed splash screen, made it look prettier

--- app/src/main/java/com/cmput301/cia/views/SplashActivity.java
+++ app/src/main/java/com/cmput301/cia/views/SplashActivity.java
@@ -16,6 +16,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_splash_screen);
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
