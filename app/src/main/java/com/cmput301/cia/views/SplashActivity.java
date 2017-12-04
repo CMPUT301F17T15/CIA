eef278023f920e3710ad6bbcc6212d384b24190e
Jessica Prieto <jessicaprieto31@gmail.com>
2017-12-04 12:52:47 -0700

    introduce splash screen and change app logo

---
+++ app/src/main/java/com/cmput301/cia/views/SplashActivity.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2017 CMPUT301F17T15. This project is distributed under the MIT license.
+ */
+
+package com.cmput301.cia.views;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cmput301.cia.R;
+import com.cmput301.cia.activities.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
